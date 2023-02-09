2a3a03605182393ae561661a3cc0adc051c22e15
Miguel Pereira <miguel.vpereira14@gmail.com>
2023-02-09 18:45:40 +0100

    Final Version

--- TP3/Statistic.cpp
+++ TP3/Statistic.cpp
@@ -1,3 +1,16 @@
+/*************************************************************************
+                           Trajet  -  description
+                             -------------------
+    début                : 25/11/2022
+    auteurs              : Miguel Pereira, Artur Pereira
+*************************************************************************/
+
+//------ Réalisation de la classe <Statistic> (fichier Statistic.cpp) --------
+
+//---------------------------------------------------------------- INCLUDE
+
+//-------------------------------------------------------- Include système
+
 #include <string>
 #include <iostream>
 #include <set>
@@ -6,24 +19,71 @@
 #include<utility>
 #include<algorithm>
 
+using namespace std;
+
+// -------------------------------------------------------- Include personnel
 #include "Statistic.h"
 #include "Log.h"
 
-using namespace std;
+// ----------------------------------------------------------------- PUBLIC
 
-Statistic::Statistic(vector<Log> logVector, bool grapheF, string grapheFi):
-logs(logVector), grapheFlag(grapheF), grapheFile(grapheFi){
-}
+// ------------------------------------------------------ Méthodes publiques
+
+void Statistic::displayCount(){
+  // Compte le nombre fois que chaque domaine est accedé
+  map<string, int> count;
+  for(int i = 0; i<(int)logs.size();i++){
+    string targetURL = logs[i].getTargetURL();
+    auto targetLocation = count.find(targetURL);
+    if(targetLocation != count.end()){
+      targetLocation->second++;
+    }
+    else{
+      count[targetURL] = 1;
+    }
+  }
+
+  // Si le graphe doit être crée, on appele la méthode generateGraphe
+  if(grapheFlag){
+    cout << "Dot-file " << grapheFile << " generated" << endl;
+    generateGraph(grapheFile);
+  }
+
+  // Création d'un std::set qui va ordonner les domaines selon comp
+  // comp est tel que les fichiers le plus accedés aparaîssent avant et, dans le
+  // cas où il y a une égalité, ils sont organisés en ordre alphabétique
+  set<pair<int,string>, decltype(&comp)> order(&comp);
+  for(auto it = count.begin(); it!=count.end(); it++){
+    order.insert(make_pair(it->second,it->first));
+  }
+
+  // Affichage des 10 premiers pages du std::set
+  int maxLines = 10, lines = 0;
+  auto it = order.begin();
+  while(lines < maxLines && it != order.end()){
+    int cur = it->first;
+    while(it != order.end() && it->first == cur){
+      cout << it->second << " (" << it->first << " hits)" << endl;
+      lines++;
+      it++;
+    }
+  }
+} // ---------- Fin de displayCount
+
+// ------------------------------------------------------ Méthodes privées
 
 void Statistic::generateGraph(string graphFileName){
+  // Associe chaque fichier à un id
   map<string, int> nodes;
+  // Graphe de la forme directed weighted edge list
   map<pair<int, int>, int> edges;
 
   unsigned int id = 0;
 
   for(Log log : logs){
     map<string, int>::iterator posRef = nodes.find(log.getReferer());
-    unsigned int idRef, idTarget; 
+    unsigned int idRef, idTarget;
+    // Si le referer n'a pas d'id, on lui donne un, sinon on le récupere 
     if(posRef != nodes.end())
       idRef = posRef->second;
     else{
@@ -31,6 +91,7 @@ void Statistic::generateGraph(string graphFileName){
       idRef = id;
     }
 
+    // Si le fichier n'a pas d'id, on lui donne un, sinon on le récupere
     map<string, int>::iterator posTarget = nodes.find(log.getTargetURL());
     if(posTarget != nodes.end())
       idTarget = posTarget->second;
@@ -39,9 +100,11 @@ void Statistic::generateGraph(string graphFileName){
       idTarget = id;
     }
 
+    // Augmente le poids de la arête du referer vers le fichier
     edges[{idRef, idTarget}]++;
   }
 
+  // Formatte les données du graphe pour que GrapheWiz puisse le lire
   ofstream gos;
   gos.open(graphFileName);
   gos << "digraph {\n";
@@ -56,46 +119,17 @@ void Statistic::generateGraph(string graphFileName){
       << edge.first.second << " [label=\"" << edge.second << "\"];\n";
   }
   gos << "}" << endl;
-}
+} // --------- Fin de generateGraph
 
-bool comp(const pair<int, string>& a, const pair<int, string>& b){
+bool Statistic::comp(const pair<int, string>& a, const pair<int, string>& b){
   if(a.first != b.first)
     return a.first > b.first;
   return a.second < b.second;
-}
+} // ----------- Fin de comp
 
-void Statistic::displayCount(){
-  map<string, int> count;
-  for(int i = 0; i<(int)logs.size();i++){
-    string targetURL = logs[i].getTargetURL();
-    auto targetLocation = count.find(targetURL);
-    if(targetLocation != count.end()){
-      targetLocation->second++;
-    }
-    else{
-      count[targetURL] = 1;
-    }
-  }
+//-------------------------------------------- Constructeurs - destructeur
 
-  if(grapheFlag){
-    cout << "Dot-file " << grapheFile << " generated" << endl;
-    generateGraph(grapheFile);
-  }
-
-  set<pair<int,string>, decltype(comp)*> order(comp);
-  for(auto it = count.begin(); it!=count.end(); it++){
-    order.insert(make_pair(it->second,it->first));
-  }
-
-  int maxLines = 10, lines = 0;
-  auto it = order.begin();
-  while(lines < maxLines && it != order.end()){
-    int cur = it->first;
-    while(it != order.end() && it->first == cur){
-      cout << it->second << " (" << it->first << " hits)" << endl;
-      lines++;
-      it++;
-    }
-  }
-}
-
+Statistic::Statistic(vector<Log> logVector, bool grapheF, string grapheFi):
+           logs(logVector), 
+           grapheFlag(grapheF), 
+           grapheFile(grapheFi){} //-------- Fin de Statistic
--- TP3/Statistic.h
+++ TP3/Statistic.h
@@ -1,6 +1,15 @@
+/*************************************************************************
+                           Statistic  -  description
+                             -------------------
+    début                : 20/01/2023
+    auteurs              : Miguel Pereira, Artur Pereira
+*************************************************************************/
+
+//---------- Interface de la classe <Statistic> (fichier Statistic.h) ---------
 #ifndef STATISTIC_H
 #define STATISTIC_H
 
+//--------------------------------------------------- Interfaces utilisées
 #include <string>
 #include <map>
 #include <vector>
@@ -9,19 +18,46 @@
 
 using namespace std;
 
+//------------------------------------------------------------------------
+// Classe responsable par compter le nombre de fois que chaque fichier est
+// consulté et afficher le 10 plus visités. Elle se charge aussi d'appliquer
+// les filtres passés en ligne de commande (extension, horaire) ainsi que
+// de créer le graphe des visites le cas échéant.  
+//------------------------------------------------------------------------
+
 class Statistic{
+// ------------------------------------------------------------------- PUBLIC 
 public:
-// TODO: pass the arguments relative to the command line options
-  Statistic(vector<Log> logVectors, bool grapheF, string GrapheFi);
+// ------------------------------------------------------ Méthodes publiques
+  void displayCount();
+  // Mode d'emploi: Affiche sur la sortie standard le nom des 10 fichier les 
+  // plus accedés ainsi que le nombre de fois que chacun a été lu.
+  // Si la flag grapheFlag est vrai, il produit un fichier GraphWiz
+
+//-------------------------------------------- Constructeurs - destructeur
+  Statistic(vector<Log> logVectors, bool grapheF, string GrapheFi = "");
+  // Mode d'emploi: Reçoit le vecteur avec les logs et la flag qui determine
+  // si le graphe doit être produit. Si oui, le nom du fichier du graphe
+  // doit aussi être saisi
+  
   ~Statistic() {};
 
-  void generateGraph(string graphFileName);
-  void displayCount();
-
+//-------------------------------------------------------------- PRIVATE
 private:
+//---------------------------------------------------------- Attributs privés
   vector<Log> logs;
   bool grapheFlag;
   string grapheFile;
+
+//---------------------------------------------------------- Méthodes privées
+  void generateGraph(string graphFileName);
+  // Mode d'emploi: Crée le fichier GraphWiz correspondant aux logs dans la
+  // variable privée logs et le sauvegarde sur un fichier de nom graphFileName
+
+  static bool comp(const pair<int, string>& a, 
+                              const pair<int, string>& b);
+  // Mode d'emploi: Comparateur utilisé pour ordonner les fichier par nombre
+  // d'accès puis, en cas d'égalité, en ordre alphabetique 
 };
 
-#endif
+#endif // STATISTIC_H
